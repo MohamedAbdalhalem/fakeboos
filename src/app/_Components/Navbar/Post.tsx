2cd605e325b78dfe1f9e1a8ae66430ce431d0b95
MohamedAbdalhalem <mohamed3ab7alem@gmail.com>
2025-06-19 02:02:44 +0300

    handle error

--- src/app/_Components/Navbar/Post.tsx
+++ src/app/_Components/Navbar/Post.tsx
@@ -22,33 +22,7 @@ import DeleteIcon from '@mui/icons-material/Delete';
 
 import { TextField,Box, Button } from '@mui/material';
 
-interface ExpandMoreProps extends IconButtonProps {
-  expand: boolean;
-}
 
-const ExpandMore = styled((props: ExpandMoreProps) => {
-  const { expand,  ...other } = props;
-  return <IconButton {...other} />;
-})(({ theme }) => ({
-  marginLeft: 'auto',
-  transition: theme.transitions.create('transform', {
-    duration: theme.transitions.duration.shortest,
-  }),
-  variants: [
-    {
-      props: ({ expand }) => !expand,
-      style: {
-        transform: 'rotate(0deg)',
-      },
-    },
-    {
-      props: ({ expand }) => !!expand,
-      style: {
-        transform: 'rotate(180deg)',
-      },
-    },
-  ],
-}));
 
 export default function Post({postDetials} : {postDetials: postType}) {
   const [expanded, setExpanded] = React.useState(false);
@@ -89,12 +63,9 @@ export default function Post({postDetials} : {postDetials: postType}) {
         <IconButton aria-label="add to favorites">
           <FavoriteIcon />
               </IconButton>
-              <ExpandMore
-          expand={false}
-          onClick={handleExpandClick}
-        >
-          <CommentIcon />
-        </ExpandMore>
+              <IconButton onClick={handleExpandClick}>
+  <CommentIcon />
+</IconButton>
         <IconButton aria-label="share">
           <ShareIcon />
         </IconButton>
